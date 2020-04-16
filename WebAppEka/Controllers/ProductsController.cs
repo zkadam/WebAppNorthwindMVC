c8917ec54ee4685c3205c3142cfeed60fe51723c
zkadam <adam.zakar@student.careeria.fi>
2020-04-16 15:16:59 +0300

    changing features for filtering

--- WebAppEka/Controllers/ProductsController.cs
+++ WebAppEka/Controllers/ProductsController.cs
@@ -10,8 +10,9 @@ namespace WebAppEka.Controllers
     public class ProductsController : Controller
     {
         // GET: Products
-        public ActionResult Index(string currentFilter1, string searchString1)
+        public ActionResult Index(string sortOrder, string currentFilter1, string searchString1)
         {
+            //--------------------------------------------------------login checking if no login, sending the action and controller name so later can return here
             if (Session["UserName"] == null)
             {
                 string actionName = this.ControllerContext.RouteData.Values["action"].ToString();
@@ -20,12 +21,34 @@ namespace WebAppEka.Controllers
             }
             else
             {
+                ViewBag.CurrentSort = sortOrder;
+
+                //tää seuraava kaks vaihe vaihtaa viewbagia elli vain sitä että onko seuraavalla klikkaamaalla ascending tai descending
+                ViewBag.ProductNameSortParm = String.IsNullOrEmpty(sortOrder) ? "productname_desc" : "";
+                ViewBag.UnitPriceSortParm = sortOrder == "UnitPrice" ? "UnitPrice_desc" : "UnitPrice";
+
                 northwindEntities db = new northwindEntities();
                 var tuotteet = from p in db.Products
                                select p;
                 if (!String.IsNullOrEmpty(searchString1))
                 {
                     tuotteet = tuotteet.Where(p => p.ProductName.Contains(searchString1));
+                }
+                //täälä sitten toteutuu filterointi
+                switch (sortOrder)
+                {
+                    case "productname_desc":
+                        tuotteet = tuotteet.OrderByDescending(p => p.ProductName);
+                        break;
+                    case "UnitPrice":
+                        tuotteet = tuotteet.OrderBy(p => p.UnitPrice);
+                        break;
+                    case "UnitPrice_desc":
+                        tuotteet = tuotteet.OrderByDescending(p => p.UnitPrice);
+                        break;
+                    default:
+                        tuotteet = tuotteet.OrderBy(p => p.ProductName);
+                        break;
                 }
 
 
