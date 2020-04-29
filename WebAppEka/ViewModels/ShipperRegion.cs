ad24ba3bd4ed6dfc219ca5266cd6eec3402e4473
zkadam <58850470+zkadam@users.noreply.github.com>
2020-04-29 14:34:08 +0300

    Feature/modal 2nd day (#4)
    * jnavbar laitettu partial viewsiksi, ViewModels are done in shippers,orders,products-joint views, 
    
    Co-authored-by: zkadam <adam.zakar@student.careeria.fi>

---
+++ WebAppEka/ViewModels/ShipperRegion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppEka.ViewModels
+{
+    public class ShipperRegion
+    {
+        public string CompanyName { get; set; }
+        public string Phone { get; set; }
+        public string RegionDescription { get; set; }
+        public int RegionID { get; set; }
+        public int ShipperID { get; set; }
+
+
+
+
+    }
+}
